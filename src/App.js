72469a27e3930662779f1d66d83a79761898d80a
abhishek <abhishekps6282@gmail.com>
2022-01-19 09:18:32 +0530

    home changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Component/Home';
+import "./Assets/css/style.css"
 
 function App() {
   return (
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -9,7 +9,11 @@ function Home() {
             <Navbar />
             <Row className="mt-4">
                 <Col xl="6">
-                    <img src={avathar} className='rounded-circle'></img>
+                    <div className="profile-image text-center">
+                        <img src={avathar} className='rounded-circle' style={{ width: "100%" }}></img>
+                        <h2 className='my-name mt-4'>Abhishek Ps</h2>
+                        <h4 className='designation'>Full Stack Developer</h4>
+                    </div>
                 </Col>
             </Row>
         </Container>
